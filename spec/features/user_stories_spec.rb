9ee3be3a435b375e7a82caa5e06a136705752113
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-11-18 15:07:49 +0000

    Does not allow planes to land when airport is full

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -5,7 +5,7 @@ describe 'User Stories' do
 # I would like to instruct a plane to land
 
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -15,8 +15,23 @@ describe 'User Stories' do
 # I would like to instruct a plane to take off
 
   it 'so planes take off from airports, instruct a plane to take off' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
   end
+
+# As an air traffic controller
+# So that I can avoid collisions
+# I want to prevent airplanes landing when my airport is full
+
+  it 'does not allow planes to land when the airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+
+  end
+
 end
